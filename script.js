84b102f35cf5b8db274a8b1e7bb641e88cf33ae6
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2022-09-07 00:04:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$.getJSON('https://SSWKCOUNSIL-WORK.kariburacyber.repl.co/get', function (data) {
+      var person = data.person;
+      for (var i = 0; i < person.length; i++) {
+        var name = person[i].name
+        var id = person[i].id
+        var room = person[i].class
+        var time = person[i].time
+        
+        var row = '<tr><th scope="row">' + name + '</th><td>' + id + '</td><td>' + room + '</td><td>' + time + '</td></tr>';
+        $('#Table').append(row);
+        //console.log(name, id, room, time)
+      }
+      //console.log(data)
+  });
